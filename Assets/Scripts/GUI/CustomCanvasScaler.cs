ae5648c77f08533a3acda68c0dc80315aaba43d5
Kirserd <Kirserd@gmail.com>
2023-09-12 19:15:50 +0200

    Custom Canvas Scaler

---
+++ Assets/Scripts/GUI/CustomCanvasScaler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class CustomCanvasScaler : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _referenceScreenSize = new(1280, 720);
+    [SerializeField]
+    private bool _scaleByHeight = true;
+    [SerializeField]
+    private float _scaleFactor = 1;
+
+    private RectTransform _rect;
+    private Vector3 _originScale;
+
+    private void Start()
+    {
+        _rect = GetComponent<RectTransform>();
+        _originScale = _rect.localScale;
+
+        RefreshScale(Screen.width, Screen.height);
+        ScreenEventListener.OnScreenSizeChanged += RefreshScale;       
+    }
+    private void RefreshScale(float width, float height)
+    {
+        Vector2 screenSizeDeviation = new Vector2(width, height) / _referenceScreenSize;
+        Debug.Log(width + " : " + height);
+        Debug.Log(screenSizeDeviation);
+        float deltaElementSize = (_scaleByHeight ? screenSizeDeviation.y : screenSizeDeviation.x) * _scaleFactor;
+        _rect.localScale = new Vector3(_originScale.x * deltaElementSize, _originScale.y * deltaElementSize, _rect.localScale.z);
+    }
+}
---
+++ Assets/Scripts/GUI/ScreenEventListener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ScreenEventListener : MonoBehaviour
+{
+    public delegate void OnScreenSizeChangedHandler(float width, float height);
+    public static OnScreenSizeChangedHandler OnScreenSizeChanged;
+
+    private float _previousWidth;
+    private float _previousHeight;
+
+    private void Update()
+    {
+        if (_previousHeight != Screen.height || _previousWidth != Screen.width)
+        {
+            _previousHeight = Screen.height;
+            _previousWidth = Screen.width;
+            OnScreenSizeChanged.Invoke(_previousWidth, _previousHeight);
+        }
+    }
+}
